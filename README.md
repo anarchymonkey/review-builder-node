0bec13d257751a9e3b4dc2e48ee7bff1b9fec708
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-11-04 10:19:20 +0530

    add basic routes and user guides

--- README.md
+++
@@ -1,2 +0,0 @@
-# review-builder-node
-A simple application using node js which builds resume for all the students
---
+++ routes/addResume.js
@@ -0,0 +1,12 @@
+/* these two lines of require are needed everytime otherwise the routes wont be linked */
+var express = require('express');
+var router = express.Router();
+/* end of important require and router lines */
+
+/* get resume page form */
+
+router.get('/',function(req,res){
+  res.render('addResume');
+});
+
+module.exports = router; // this code is essential and has to be kept same, cause express-generator needs it.
