034b12f60fa4eca115698e3b36bc131348fe1298
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-11-04 16:15:18 +0530

    add resume render template with styling

--- routes/viewResume.js
+++ routes/viewResume.js
@@ -9,6 +9,8 @@ let skills;
 let hobbies;
 let experience;
 let compact;
+let image;
+var skillarr;
 /* get resume page form */
 
 router.post('/',function(req,res){
@@ -20,13 +22,25 @@ edu = req.body.education;
 skills = req.body.skills;
 hobbies = req.body.hobbies;
 experience = req.body.experience;
+image = req.body.image;
 
+skillsarr = req.body.skills.split(' ');
+
+console.log(skillsarr[3]);
 res.redirect('/');
 });
 
 router.get('/',function(req,res){
-  res.render('viewResume',{name : name,
-                            phone : phone,email : email, edu : edu,skills:skills,hobbies:hobbies,experience:experience});
+  res.render('viewResume',{
+                            name : name,
+                            phone : phone,
+                            email : email,
+                            edu : edu,
+                            skillsarr:skillsarr,
+                            hobbies:hobbies,
+                            experience:experience,
+                            image:image
+                          });
 });
 
 module.exports = router;
